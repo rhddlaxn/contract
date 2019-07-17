64e3a52ecf0072d1b6f1ad41b6be742f37a13013
root <root@localhost.localdomain>
2019-07-17 14:44:03 +0900

    auth 실습

--- helloworld.cpp
+++ helloworld.cpp
@@ -7,11 +7,13 @@ CONTRACT helloworld: public contract {
         using contract::contract;
 
         ACTION hi(name user){
-            check(has_auth(user), "missing authhority"); 
+            
+            check(has_auth(get_self() ),"ㄲㅈ");
+            print("hello, ", user);
             //require_auth(user); === 인증된사용자아니면 에러 
 
             //has_auth() -> T or F 구별용으로쓰임
-            print("hello, ",user);
+            
         }
 
     private:
